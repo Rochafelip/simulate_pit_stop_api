d4c474f7ada7cb9c156cde36b3066ce577e7e5bb
Felipe <rocha.felipe98@gmail.com>
2025-07-18 17:57:57 -0300

    Adicionando .env no Frontend e na raiz. Retirando o Api_only para ter acesso a documentação

--- backend/app/serializers/api/v1/track_detail_serializer.rb
+++ backend/app/serializers/api/v1/track_detail_serializer.rb
@@ -1,15 +1,15 @@
 module Api
-    module V1
-    class TrackDetailsSerializer < ActiveModel::Serializer
-        attributes :id, :name, :distance, :country, :elevation_track, :number_of_curves, :created_at, :updated_at
+  module V1
+    class TrackDetailSerializer < ActiveModel::Serializer
+      attributes :id, :name, :distance, :country, :elevation_track, :number_of_curves, :created_at, :updated_at
 
-        def created_at
+      def created_at
         object.created_at.strftime("%d/%m/%Y %H:%M")
-        end
+      end
 
-        def updated_at
+      def updated_at
         object.updated_at.strftime("%d/%m/%Y %H:%M")
-        end
+      end
     end
-    end
+  end
 end
